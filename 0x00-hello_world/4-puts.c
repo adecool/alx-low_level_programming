89cc4369364ee97e18fec0b0f63938ef6e79163f
adecool <adebiyi17@gmail.com>
2022-10-13 12:54:32 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- *
- * Description: print a string
- *
  * Return - Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
