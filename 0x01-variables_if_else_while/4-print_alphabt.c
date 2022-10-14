3db2c3130ad96a546bd00769d4e23c42e24d4f69
adecool <adebiyi17@gmail.com>
2022-10-14 15:44:57 -0700

    m

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if (alpha == 'q' || alpha == 'e')
+		{
+			alpha++;
+		}
+		else
+		{
+			putchar(alpha);
+			alpha++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
