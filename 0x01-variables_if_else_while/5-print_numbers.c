cbbb8354067c686a4129bfe3d13fbafeb52317cc
adecool <adebiyi17@gmail.com>
2022-10-14 15:50:04 -0700

    m

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		printf("%d", num);
+		num++;
+	}
+	printf("\n");
+
+	return (0);
+}
