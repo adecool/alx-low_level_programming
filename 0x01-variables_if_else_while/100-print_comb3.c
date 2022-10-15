706fd0ff8a4a225f247654cdb8136a06aa2fc5f6
adecool <adebiyi17@gmail.com>
2022-10-14 17:02:32 -0700

    m

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int tens;
+	int ones;
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (ones = (tens + 1); ones <= '9'; ones++)
+		{
+			putchar(tens);
+			putchar(ones);
+
+			if (tens != '8' || ones != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
