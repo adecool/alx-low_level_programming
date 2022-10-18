f876deeb479dc06d6da81c38267e50fe19bc148f
adecool <adebiyi17@gmail.com>
2022-10-18 13:18:48 -0700

    m

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * Return: 1 or 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
