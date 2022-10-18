3ab9a68e642a0762387c78861d8fd83dc3d0de7c
adecool <adebiyi17@gmail.com>
2022-10-18 10:02:33 -0700

    m

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * print_alphabet - Entry point
+ * print_alphabet_x10 - Entry point
  * Return: void
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int i;
 	char c = 'a';
