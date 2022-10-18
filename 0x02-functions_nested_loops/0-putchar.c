06351ed605349c1ce030e50c540890b3eb6ed682
adecool <adebiyi17@gmail.com>
2022-10-17 22:52:22 -0700

    m

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include main.h
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char s[] = "_putchar\n";
+
+	for (int i = 0; i <= 8; i++)
+	{
+		_putchar(s[i]);
+	}
+
+	return (0);
+}
