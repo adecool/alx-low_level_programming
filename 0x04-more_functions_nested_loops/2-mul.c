74026c7874fb36c575e45ad4018c769aff6a474c
adecool <adebiyi17@gmail.com>
2022-10-20 08:25:58 -0700

    m

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ * @a: first integer checked
+ * @b: second integer checked
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	j = a * b;
+
+	putchar(j);
+
+	return (0);
+}
