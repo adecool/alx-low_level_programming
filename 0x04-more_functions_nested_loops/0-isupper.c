eb9249a6556ee9c82ae4ffef689a8c34cceb75ed
adecool <adebiyi17@gmail.com>
2022-10-20 05:56:42 -0700

    m

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - check if upper case
+ * @c: character to check
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	int c;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
